562a815bc1b608a6832d50b5f7f767c1316159d4
Monokame <jeremy.marc.pro@gmail.com>
2018-12-04 15:46:03 +0100

    Ajoutez des fichiers projet.

---
+++ Station Météo Projet 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Station_Météo_Projet_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+
+
+        }
+
+    }
+}
